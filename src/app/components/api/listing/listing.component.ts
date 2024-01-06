ebc6d134ffac37779f877ab85c7e1c6418d86268
John Akello <john.akello@equitybank.co.ke>
2024-01-06 11:16:04 +0300

    Api Listing sample content setup
    1. Api Listing sample content setup with generic place holders
    2. Mixins added to allow border radius creation & box shadows

--- src/app/components/api/listing/listing.component.ts
+++ src/app/components/api/listing/listing.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.scss']
 })
-export class ListingComponent {
+export class ListingComponent implements OnInit{
 
+  @Input() title:string = '';
+  @Input() description:string = '';
+  @Input() routeLink:string ='';
+  @Input() stylingClass:string = 'one_quarter';
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  
 }
