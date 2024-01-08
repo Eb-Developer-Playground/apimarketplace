ebb83901aa3c287dcd5e825f8699bc7f9565580b
John Akello <john.akello@equitybank.co.ke>
2024-01-08 10:35:07 +0300

    Processor component inputs defined
    1. Processor component inputs defined
    2. Api catalogue comments setup

--- src/app/shared/processor/processor.component.ts
+++ src/app/shared/processor/processor.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-processor',
   templateUrl: './processor.component.html',
   styleUrls: ['./processor.component.scss']
 })
-export class ProcessorComponent {
+export class ProcessorComponent implements OnInit {
+
+  @Input() processingText:string = 'Processing';
+  @Input() diameter:number = 100;
+  @Input() strokeWidth:number = 3;
+
+  ngOnInit():void{
+
+
+  }
+
+
 
 }
