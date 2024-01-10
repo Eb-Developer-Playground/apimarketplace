cc100d5b73f3568f5d338cd7d2bcf32fc98d937b
John Akello <john.akello@equitybank.co.ke>
2024-01-10 10:23:50 +0300

    Featured Api loaded and added to home apge
    1. Featured Api listing added to home page and slimmed to 3
    2. Home page breakers and layout setup

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,18 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import product service to enable product fetch
+import { ProductServiceService } from 'src/app/services/product/product-service.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers:[
+    ProductServiceService
+  ]
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // Property declarations
+  apiListing:any = [];
+  loading:boolean = true;
+
+  constructor(
+    private productService:ProductServiceService
+  ) { 
+
+
+  }
 
   ngOnInit(): void {
 
-    
+    // On initialisation fetch all api listings for this view
+    this.fetchApiListing();
 
+  }
+
+  fetchApiListing(){
+
+    this.productService.getApiProducts($localize.locale).subscribe((data) => {
+
+
+      if(data !== ""){
+
+        this.apiListing = data;
+        this.loading = false;
+        
+      }
+
+    },(error) =>{
+
+      console.log("Error handling ", error);
+      // Handle Error here
+      this.loading = false;
+
+    });
   }
 
 }
