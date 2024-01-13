4f14c6b7ad5c511daa6283a9af9b316d4aa68321
John Akello <john.akello@equitybank.co.ke>
2024-01-13 11:18:00 +0300

    Create account unit tests setup

--- src/app/views/auth/createaccount/createaccount.component.spec.ts
+++ src/app/views/auth/createaccount/createaccount.component.spec.ts
@@ -8,6 +8,21 @@ describe('CreateaccountComponent', () => {
   let component: CreateaccountComponent;
   let fixture:ComponentFixture<CreateaccountComponent>;
 
+  let customerNameInput:HTMLInputElement;
+  let customerErrorMessage:HTMLElement;
+
+  let userNameInput:HTMLInputElement;
+  let userNameErrorMessage:HTMLElement;
+
+  let emailInput:HTMLInputElement;
+
+  let submitButton:HTMLElement;
+
+  let accountTitle:HTMLElement
+
+
+
+
   
   beforeEach(async () => {
 
@@ -19,6 +34,14 @@ describe('CreateaccountComponent', () => {
     fixture = TestBed.createComponent(CreateaccountComponent);
 
     component = fixture.componentInstance;
+    
+    customerNameInput = fixture.nativeElement.querySelector('input#customerName');
+
+    userNameInput = fixture.nativeElement.querySelector('input#userName');
+
+    emailInput = fixture.nativeElement.querySelector('input#emailAddress');
+
+    submitButton = fixture.nativeElement.querySelector('.createAccount');
 
     fixture.detectChanges();
 
@@ -33,35 +56,71 @@ describe('CreateaccountComponent', () => {
 
   it('Should have account creation title',() => {
 
-    const accountTitle:HTMLElement = fixture.nativeElement;
+    accountTitle = fixture.nativeElement;
 
     expect(accountTitle.querySelector('h1')?.textContent).toContain('Account creation page');
 
   });
 
-  xit('Customer name validation field should be present on emtpy field',() => {
+  it('Customer name validation field should be present on emtpy field',() => {
+
+    customerErrorMessage = fixture.nativeElement.querySelector('.customerNameError');
+
+    expect(customerErrorMessage.textContent).toBe('** Customer Name Required');
+
+  });
+
+  it('Customer name validation for fields less than 4 letters',() => {
+
+    component.createCustomerForm.patchValue({
+      customerName:'Kel'
+    });
+
+    fixture.detectChanges();
+
+    customerErrorMessage = fixture.nativeElement.querySelector('.customerNameLength');
+
+    expect(customerErrorMessage.textContent).toBe('** Customer Name should be at least 4 characters');
+
+  });
+
+  it('Invalid Email validation confirmation',() => {
+
+    component.createCustomerForm.patchValue({
+      emailAddress:'emailInput'
+    });
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.emailInvalid').textContent).toBe('** Email format invalid');
 
 
   });
 
-  xit('Customer name validation for fields less than 4 letters',() => {
+  it('On user creation, success message to appear after 4 seconds',() => {
+
+    component.createCustomerForm.patchValue({
+      customerName:'Kelvin Waweru',
+      userName:'kelvinwaweru',
+      emailAddress:'kelvin@equitybank.co.ke',
+      phoneNumber:'254719136031',
+      password:'12345'
+    });
+
+    fixture.detectChanges();
+
+    submitButton = fixture.nativeElement.querySelector('.createAccount');
+
+    submitButton.click();
+
+    setTimeout(() => {
+
+      expect(fixture.nativeElement.querySelector('.success h2').textContent).toBe('Your account has been created succesfully');
+
+    },5000);
 
 
   });
 
-  xit('Customer name validation for fields less than 4 letters',() => {
-
-
-  });
-
-  xit('On user creation, success message to appear after 4 seconds',() => {
-
-
-  });
-
-  xit('On click of login, user to be redirected to login page',() => {
-
-
-  });
 
 });
